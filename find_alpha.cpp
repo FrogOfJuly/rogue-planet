3704ec76e32829316dee80c4a0d44175453e7fa8
Максим Тимохин <maximtim@outlook.com>
2021-04-28 16:16:29 +0300

    find_alpha

--- find_alpha.cpp
+++ find_alpha.cpp
@@ -2,26 +2,25 @@
 
 #include <iostream>
 
-bool check_alpha(engine eng, double alpha, double eps) {
+bool check_alpha(engine eng, double alpha, double eps, size_t num_steps) {
     eng.enable_radiation(alpha);
     static const double wanted_temp = 300;
     auto old_t = eng.getT().back();
     eng.forward_pass();
     eng.backward_pass();
     auto curr_t = eng.getT().back();
-    while (abs(old_t - curr_t) > eps) {
+    for (size_t i = 0; i < num_steps; ++i) {
         eng.forward_pass();
         eng.backward_pass();
         old_t = curr_t;
         curr_t = eng.getT().back();
     }
-    std::cout << curr_t << std::endl;
     return abs(curr_t - wanted_temp) < eps;
 }
 
-double find_alpha(engine eng, double step, double eps) {
-    for (double alpha = step; alpha <= 1; alpha += step) {
-        if (check_alpha(eng, alpha, eps)) {
+double find_alpha(engine eng, double step, double eps, size_t num_steps) {
+    for (double alpha = 0; alpha <= 1; alpha += step) {
+        if (check_alpha(eng, alpha, eps, num_steps)) {
             return alpha;
         }
     }
