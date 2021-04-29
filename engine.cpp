166495bdf1bb0332ae7815de8ffbd95b78853f91
frogofjuly <golubev.ki@phystech.edu>
2021-04-29 20:51:05 +0300

    some tweaks

--- engine.cpp
+++ engine.cpp
@@ -7,99 +7,152 @@
 #include <iostream>
 
 void print_array(const engine::grid &array) {
+    return;
     for (auto &t : array) {
         std::cout << t << ", ";
     }
     std::cout << std::endl;
 }
 
-void engine::forward_pass() {
-    double F_0 = T0;
-    double B_0 = 1;
-    double C_0 = 0;
-    alpha[0] = -C_0 / B_0;
-    beta[0] = F_0 / B_0;
-    for (int k = 1; k < N(); ++k) {
-        double A = r() + spherical_amend(k) * r();
-        double B = -1 - 2 * r() - spherical_amend(k) * 2 * r();
-        double C = A;
-        double F = -T[k - 1];
-        alpha[k] = -C / (A * alpha[k - 1] + B);
-        beta[k] = (F - A * beta[k - 1]) / (A * alpha[k - 1] + B);
-    }
-}
-
-void engine::backward_pass() {
-    double B_N = S * sigma * (T.back() * T.back() * T.back()) * dx + 1;
-    double A_N = -1;
-    double F_N = 0;
-    T[N()] = (F_N - A_N * beta[N() - 1]) / (A_N * alpha[N() - 1] + B_N);
-    for (int k = N() - 1; k >= 0; --k) {
-        T[k] = alpha[k] * T[k + 1] + beta[k];
-    }
-}
-
-engine::grid engine::getT() const {
-    std::vector<double> res;
-    for (auto t : T) {
-        res.push_back(t);
-    }
-    return res;
-}
-
-void engine::fill_with(double t) {
-    for (int i = 1; i < N() + 1; ++i) {
-        T[i] = t;
-    }
-}
-
-void engine::enable_radiation(double alpha) {
-    sigma = alpha * sigma_sf;
-}
-
-engine::engine(double dx, double dt) : dt(dt), dx(dx) {
+engine::engine(double dx, double dt) {
+    eval_params.dx = dx;
+    eval_params.dt = dt;
 }
 
 void engine::init_physics(double mantle_temerature,
+                          double surface_temperature,
                           double thermal_conductivity,
                           double specific_heat_capacity,
                           double density,
                           double upper_mantle,
-                          double troposphere_height) {
-    T0 = mantle_temerature;
-    R_c = upper_mantle;
-    R_t = troposphere_height;
-    dx /= (R_t - R_c);
-    std::cout << "point number: " << N() << std::endl;
-    std::cout << "dx: " << dx << std::endl;
-    alpha = std::vector<double>(N() + 1);
-    beta = std::vector<double>(N() + 1);
-    T = std::vector<double>(N() + 1);
-    kappa = thermal_conductivity;
-    c_p = specific_heat_capacity;
-    rho = density;
-    S = 4 * 3.14 * R_e * R_e;
+                          double troposphere_height,
+                          double interval_to_model) {
+    phys_params.T0 = mantle_temerature;
+    phys_params.R_c = upper_mantle;
+    phys_params.R_t = troposphere_height;
+    phys_params.Ts = surface_temperature;
 
+    phys_params.core.kappa = thermal_conductivity;
+    phys_params.core.c = specific_heat_capacity;
+    phys_params.core.rho = density;
+
+
+    eval_params.L = phys_params.R_t - phys_params.R_c;
+    eval_params.l = interval_to_model;
+
+    phys_params.Tl = phys_params.T0 + (phys_params.Ts - phys_params.T0) /
+                                      eval_params.L * (eval_params.L - eval_params.l);
+
+    eval_params.N = (int) (eval_params.l / eval_params.dx);
+
+    eval_params.J0 = phys_params.T0 * phys_params.T0 * phys_params.T0 * phys_params.sigma /
+                     phys_params.core.kappa;
+    eval_params.r = eval_params.dt / (eval_params.dx * eval_params.dx);
+
+
+    alpha = std::vector<double>(eval_params.N + 1);
+    beta = std::vector<double>(eval_params.N + 1);
+    T = std::vector<double>(eval_params.N + 1);
 }
 
-size_t engine::N() const {
-    return size_t(1.0 / dx);
+void engine::fill_with(double t) {
+    T[0] = 1;
+    for (int i = 1; i < eval_params.N + 1; ++i) {
+        T[i] = t / phys_params.T0;
+    }
 }
 
-double engine::r() const {
-    const double k = (rho * c_p / kappa);
-    const double alp = (R_t - R_c);
-    return dt / (dx * dx) * k / (alp * alp);
+void engine::enable_radiation(double alpha_) {
+    phys_params.sigma = consts.sigma_sb;
+    eval_params.J0 = alpha_ * phys_params.T0 * phys_params.T0 * phys_params.T0 * phys_params.sigma /
+                     phys_params.core.kappa;
 }
 
-double engine::x(int i) const {
-    return dx * i;
+void engine::forward_pass() {
+    {
+        double F_0 = phys_params.Tl / phys_params.T0;
+        double B_0 = 1.0;
+        double C_0 = 0.0;
+        alpha[0] = -C_0 / B_0;
+        beta[0] = F_0 / B_0;
+    }
+    print_array(T);
+//    std::cout << "***********************************" << std::endl;
+    for (int k = 1; k < eval_params.N; ++k) {
+//        auto p = get_params(k);
+//        double amend = p.spherical_amend;
+        double amend = 1.0 / (consts.R_e - eval_params.l + eval_params.dx * k);
+        double r = eval_params.r;
+
+        double A = r + amend * r;
+        double B = -1 - 2 * r - amend * 2 * r;
+        double C = A;
+        double F = -T[k - 1];
+
+        alpha[k] = -C / (A * alpha[k - 1] + B);
+        beta[k] = (F - A * beta[k - 1]) / (A * alpha[k - 1] + B);
+    }
+    print_array(alpha);
+    print_array(beta);
 }
 
-double engine::spherical_amend(int k) const {
-    const double alp = (R_t - R_c);
-    const double bet = (R_e + R_c);
-//    return 0;
-    return dx / (2 * (x(k) + bet / alp));
+void engine::backward_pass() {
+    double B_N =
+            eval_params.J0 * (T.back() * T.back() * T.back()) *
+            eval_params.dx + 1;
+//    std::cout << "J0: " << eval_params.J0 << std::endl;
+    double A_N = -1;
+    double F_N = 0;
+    int N = eval_params.N;
+    T[N] = (F_N - A_N * beta[N - 1]) / (A_N * alpha[N - 1] + B_N);
+    for (int k = eval_params.N - 1; k >= 0; --k) {
+        T[k] = alpha[k] * T[k + 1] + beta[k];
+    }
+    eval_params.cur_time += eval_params.dt;
+//    std::cout << "-------------------------------" << std::endl;
+    print_array(T);
+//    std::cout << "===============================" << std::endl;
+}
+
+engine::grid engine::getT() const {
+    std::vector<double> res(T.size());
+    for (int i = 0; i < eval_params.N + 1; ++i) {
+        res[i] = T[i] * phys_params.T0;
+    }
+    return res;
+}
+
+engine::grid engine::getX() const {
+    grid res(T.size());
+    for (int i = 0; i < eval_params.N + 1; ++i) {
+        res[i] = (-eval_params.l + eval_params.dx * i) / 1000.0;
+    }
+    return res;
+}
+
+double engine::get_passed_time() const {
+    double real_t = eval_params.cur_time * (phys_params.core.c * phys_params.core.rho) / phys_params.core.kappa;
+    return real_t;
+}
+
+engine::grid engine::simulate_n_steps(int n) {
+    for (int i = 0; i < n; ++i) {
+        forward_pass();
+        backward_pass();
+    }
+    return getT();
+}
+
+
+int engine::ticks_per_day() const {
+    return int(3600.0 * 24.0 / (eval_params.dt * (phys_params.core.c * phys_params.core.rho) / phys_params.core.kappa));
+}
+
+void engine::set_border_cond(const engine::grid &data) {
+    assert(T.size() - 1 == data.size());
+    T[0] = 1.0;
+    for (size_t i = 0; i < T.size(); ++i) {
+        T[i + 1] = data[i] / phys_params.T0;
+    }
 }
 
--- python/test.py
+++ python/test.py
@@ -1,7 +1,6 @@
 import earthcooling
 import matplotlib.pyplot as plt
-from tqdm import tqdm
-import numpy as np
+from tqdm import tqdm, trange
 
 
 def reset_lim():
@@ -9,54 +8,91 @@ def reset_lim():
     axes.set_ylim([0, 1000])
 
 
-def model(eng, step_num=500, draw_each=50):
-    plt.ion()
-    for _ in tqdm(range(int(step_num/draw_each))):
-        eng.simulate_n_steps(draw_each)
-        data = eng.getT()
+def get_info(eng):
+    time = eng.passed_time()
+    axes = plt.gca()
+    day = time // (24 * 3600)
+    time = time % (24 * 3600)
+    hour = time // 3600
+    time %= 3600
+    minutes = time // 60
+    time %= 60
+    seconds = time
 
+    return {"days": day, "T_surf": eng.getT()[-1]}
+
+
+def model(eng, step_num=500, draw_each=50):
+    surface = []
+    plt.ion()
+    t = trange(int(step_num / draw_each), desc='days passed', leave=True)
+    for _ in t:
+        eng.simulate_n_steps(draw_each)
+
+        t.set_description(str(get_info(eng)))
+
+        temps = eng.getT()
+        surface.append(temps[-1])
+
+        plt.plot(eng.getX(), temps)
         reset_lim()
-        plt.plot(data)
 
         plt.draw()
         plt.pause(0.1)
         plt.clf()
 
     plt.ioff()
+    return surface
 
 
 def plot_eng(eng):
-    plt.plot(eng.getT())
+    plt.plot(eng.getX(), eng.getT())
     reset_lim()
     plt.show()
 
 
 if __name__ == "__main__":
-    eng = earthcooling.Engine(0.1, 0.01)
+    dx = 0.01
+    dt = pow(10, -1)
+    eng = earthcooling.Engine(dx, dt)
+
+    Tsuf = 380.0
+    depth = 5 * pow(10, 2)
+    T0 = 900
+    # args = {
+    #     "mantle_temperature": T0,
+    #     "surface_temperature": Tsuf,
+    #     "thermal_conductivity": 3.75,
+    #     "specific_heat_capacity": 1077.0,
+    #     "density": 2700.0,
+    #     "upper_mantle": -40 * pow(10, 3),
+    #     "troposphere_height": 0,  # 15*pow(10, 3)
+    #     "interval_to_model": 1 * pow(10, 3)
+    # }
     args = {
-        "mantle_temperature": 573,
+        "mantle_temperature": T0,
+        "surface_temperature": Tsuf,
         "thermal_conductivity": 3.75,
         "specific_heat_capacity": 1077.0,
         "density": 2700.0,
         "upper_mantle": -40 * pow(10, 3),
-        "troposphere_height": 0  # 15*pow(10, 3)
+        "troposphere_height": 0.0,  # 15*pow(10, 3)
+        "interval_to_model": depth
     }
     eng.init_physics(**args)
-
-    eng.fill_with(300.0)
-
-    eng.forward_pass()
-    eng.backward_pass()
+    N = eng.N()
+    eng.fill_with(Tsuf)
+    Tl = eng.Tl()
+    print("left border tmp: ", Tl)
+    eng.set_border_cond([Tl + (Tsuf - Tl) * float(i) / float(N) for i in range(N)])
+    ticks_per_day = eng.ticks_per_day()
+    eng.enable_radiation(0.2)
+    model(eng, step_num=10000, draw_each=ticks_per_day*100)
+    plot_eng(eng)
+    eng.enable_radiation(1.0)
+    surface_tmp = model(eng, step_num=5000, draw_each=ticks_per_day)  # ticks_per_day + 1)
 
     plot_eng(eng)
 
-    model(eng, step_num=60000, draw_each=1000)
-
-    print("balance?")
-    plot_eng(eng)
-
-    eng.enable_radiation(0.5)
-
-    model(eng, step_num=10000, draw_each=500)
-
-    plot_eng(eng)
+    plt.plot(surface_tmp)
+    plt.show()
